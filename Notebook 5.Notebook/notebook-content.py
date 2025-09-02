356799583a86a21bcc39ae3aaedfb2815877cecf
Kalistauy <104914226+Kalistauy@users.noreply.github.com>
2025-09-01 22:20:00 -0300

    Committing 2 items from workspace 65b0ce32-32d0-4180-9cff-39039acfc19c

---
+++ Notebook 5.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "a34a69ea-9b15-4788-b686-39ffdb915f78",
+# META       "default_lakehouse_name": "Lakehouse_Last",
+# META       "default_lakehouse_workspace_id": "65b0ce32-32d0-4180-9cff-39039acfc19c",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "a34a69ea-9b15-4788-b686-39ffdb915f78"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
